0b587c5d509a94691787f302a090e03ce8551602
sibeardev <82308935+sibeardev@users.noreply.github.com>
2025-04-07 17:51:45 +0300

    feat: saves chat conversations

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
 import logging
+from datetime import datetime
 
+import aiofiles
 from environs import Env
 
 import gui
@@ -12,7 +14,9 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-async def read_msgs(host: str, port: int, queue: asyncio.Queue):
+async def read_msgs(
+    host: str, port: int, messages_queue: asyncio.Queue, history_queue: asyncio.Queue
+):
     try:
         reader, writer = await asyncio.open_connection(host, port)
 
@@ -23,7 +27,10 @@ async def read_msgs(host: str, port: int, queue: asyncio.Queue):
                     break
 
                 message_text = message_bytes.decode().rstrip()
-                queue.put_nowait(message_text)
+                timestamp = datetime.now().strftime("[%d.%m.%y %H:%M] ")
+                message = f"{timestamp}{message_text}"
+                messages_queue.put_nowait(message)
+                history_queue.put_nowait(message)
 
             except (ConnectionError, asyncio.IncompleteReadError) as e:
                 logger.error(f"Connection error: {e}")
@@ -38,18 +45,47 @@ async def read_msgs(host: str, port: int, queue: asyncio.Queue):
         logger.debug("Connection closed")
 
 
+async def save_messages(filepath: str, queue: asyncio.Queue):
+    async with aiofiles.open(filepath, mode="a", encoding="utf-8") as history_file:
+        while True:
+            message = await queue.get()
+            try:
+                await history_file.write(f"{message}\n")
+                await history_file.flush()
+            except Exception as e:
+                logger.error(f"Error when saving a message: {e}")
+
+
+async def load_chat_history(filepath: str, queue: asyncio.Queue):
+    try:
+        async with aiofiles.open(filepath, mode="r", encoding="utf-8") as history_file:
+            async for line in history_file:
+                cleaned_line = line.strip()
+                if cleaned_line:
+                    await queue.put(cleaned_line)
+    except FileNotFoundError:
+        logger.warning(f"History file not found: {filepath}")
+    except Exception as e:
+        logger.error(f"Error when loading history: {str(e)}")
+
+
 async def main():
     env = Env()
     env.read_env()
     host = env.str("HOST")
     port = env.int("CHAT_PORT", 5000)
+    filepath = "minechat.history"
 
     messages_queue = asyncio.Queue()
     sending_queue = asyncio.Queue()
     status_updates_queue = asyncio.Queue()
+    history_queue = asyncio.Queue()
+
+    await load_chat_history(filepath, messages_queue)
     await asyncio.gather(
         gui.draw(messages_queue, sending_queue, status_updates_queue),
-        read_msgs(host, port, messages_queue),
+        read_msgs(host, port, messages_queue, history_queue),
+        save_messages(filepath, history_queue),
     )
 
 

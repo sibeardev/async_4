fe952a57d956241b0e85aa9d1a4489617f16e376
sibeardev <82308935+sibeardev@users.noreply.github.com>
2025-04-10 11:58:11 +0300

    feat: replaced gather with create_task_group

--- gui.py
+++ gui.py
@@ -4,6 +4,8 @@ from enum import Enum
 from tkinter import messagebox
 from tkinter.scrolledtext import ScrolledText
 
+import anyio
+
 
 class TkAppClosed(Exception):
     pass
@@ -134,12 +136,10 @@ async def draw(messages_queue, sending_queue, status_updates_queue):
 
     conversation_panel = ScrolledText(root_frame, wrap="none")
     conversation_panel.pack(side="top", fill="both", expand=True)
-
-    await asyncio.gather(
-        update_tk(root_frame),
-        update_conversation_history(conversation_panel, messages_queue),
-        update_status_panel(status_labels, status_updates_queue),
-    )
+    async with anyio.create_task_group() as tg:
+        tg.start_soon(update_tk, root_frame)
+        tg.start_soon(update_conversation_history, conversation_panel, messages_queue)
+        tg.start_soon(update_status_panel, status_labels, status_updates_queue)
 
 
 def show_message_box(title, message):
